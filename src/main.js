1555ba3a3ec5a176e5c8efaee7e0976c761d1290
mihailgaberov <mihail.gaberov@gmail.com>
2024-07-07 11:46:44 +0200

    :recycle: extract router to a separated file

--- src/main.js
+++ src/main.js
@@ -1,26 +1,7 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+
 import App from './App.vue'
-import ViewNotes from '@/views/ViewNotes.vue'
-import ViewStats from '@/views/ViewStats.vue'
-
-
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'notes',
-            component: ViewNotes
-        },
-        {
-            path: '/stats',
-            name: 'stats',
-            component: ViewStats
-        },
-    ]
-})
-
+import router from './router'
 
 createApp(App)
     .use(router)
