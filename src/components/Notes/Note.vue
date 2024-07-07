988698a1ef58912f7de2ef2ac75445e9b5ec6263
mihailgaberov <mihail.gaberov@gmail.com>
2024-07-07 14:11:47 +0200

    :sparkles: add chars counter

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -4,6 +4,9 @@
             <div class="content">
                 {{ note.content }}
             </div>
+            <div class="has-text-right has-text-grey-light m-2">
+                <small>{{ characterLength }}</small>
+            </div>
         </div>
         <footer class="card-footer">
             <a href="#" class="card-footer-item">Edit</a>
@@ -13,10 +16,18 @@
 </template>
 
 <script setup>
-const { props } = defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
     note: {
         type: Object,
         required: true,
     }
-});
+})
+
+const characterLength = computed(() => {
+    const length = props.note.content.length;
+    const description = length > 1 ? 'characters' : 'character';
+    return `${length} ${description}`
+})
 </script>
