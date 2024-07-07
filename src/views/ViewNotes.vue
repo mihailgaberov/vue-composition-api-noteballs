173f85d738194c09b70b85ef44c8dc4fd8588c82
mihailgaberov <mihail.gaberov@gmail.com>
2024-07-07 12:21:16 +0200

    :sparkles: adding card components

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,25 @@
 <template>
     <div class="notes">
-        <h1>Notes</h1>
+        <div class="card mb-4" v-for="i in 3">
+            <p class="card-header-title">Component</p>
+            <button class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+            </button>
+            <div class="card-content">
+                <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                    iaculis mauris.
+                    <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br />
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
     </div>
 </template>
