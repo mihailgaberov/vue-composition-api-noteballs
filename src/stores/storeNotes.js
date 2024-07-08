537468d8c4cca24913bcabfc70779abd19d1c42d
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-08 17:33:06 +0200

    :sparkles: add methods for getting the number of notes and chars from store

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,41 +1,48 @@
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
 
-export const useNotesStore = defineStore('storeNotes', {
-    state: () => ({
-        notes: [
-            {
-                id: '1',
-                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
-            },
-            {
-                id: '2',
-                content: 'This is shorter.',
-            }
-        ]
-    }),
-    actions: {
-        addNote(note) {
-            const id = new Date().getTime().toString()
+export const useNotesStore = defineStore("storeNotes", {
+  state: () => ({
+    notes: [
+      {
+        id: "1",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+      },
+      {
+        id: "2",
+        content: "This is shorter.",
+      },
+    ],
+  }),
+  actions: {
+    addNote(note) {
+      const id = new Date().getTime().toString();
 
-            this.notes.unshift({
-                id,
-                content: note
-            });
-        },
-        deleteNote(idToDelete) {
-            this.notes = this.notes.filter(({ id }) => id !== idToDelete)
-        },
-        updateNoteContent({ id, content }) {
-            const index = this.notes.findIndex((note) => note.id === id)
-            this.notes[index].content = content
-        }
+      this.notes.unshift({
+        id,
+        content: note,
+      });
     },
-    getters: {
-        getNoteContent: (state) => {
-            return (id) => {
-                const note = state.notes.find((note) => note.id === id)
-                return note ? note.content : ''
-            }
-        }
-    }
-})
+    deleteNote(idToDelete) {
+      this.notes = this.notes.filter(({ id }) => id !== idToDelete);
+    },
+    updateNoteContent({ id, content }) {
+      const index = this.notes.findIndex((note) => note.id === id);
+      this.notes[index].content = content;
+    },
+  },
+  getters: {
+    getNoteContent: (state) => {
+      return (id) => {
+        const note = state.notes.find((note) => note.id === id);
+        return note ? note.content : "";
+      };
+    },
+    totalNotesCount: (state) => {
+      return state.notes.length;
+    },
+    totalCharactersCount: (state) => {
+      return state.notes.reduce((acc, { content }) => acc + content.length, 0);
+    },
+  },
+});
--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -1,5 +1,28 @@
 <template>
     <div class="stats">
-        <h1>Stats</h1>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>Stat</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Number of Notes</td>
+                    <td>{{ notesStore.totalNotesCount }}</td>
+                </tr>
+                <tr>
+                    <td>Number of Characters (of all notes)</td>
+                    <td>{{ notesStore.totalCharactersCount }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
+
+<script setup>
+import { useNotesStore } from '@/stores/storeNotes';
+const notesStore = useNotesStore();
+
+</script>
