8bd7fc0a70d181d3b6994a28833bea5da2a8abec
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-08 17:24:02 +0200

    :recycle: close menu when click on a menu item

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -18,10 +18,10 @@
 
             <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
                 <div class="navbar-end">
-                    <RouterLink to="/" class="navbar-item" active-class="is-active">
+                    <RouterLink @click="showMobileNav = false" to="/" class="navbar-item" active-class="is-active">
                         Notes
                     </RouterLink>
-                    <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+                    <RouterLink @click="showMobileNav = false" to="/stats" class="navbar-item" active-class="is-active">
                         Stats
                     </RouterLink>
                 </div>
