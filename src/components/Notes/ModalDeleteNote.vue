d6885c9fb8e0490bca1e9d049e72446215113875
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-11 13:51:51 +0200

    :worker: adding delete modal

---
+++ src/components/Notes/ModalDeleteNote.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Delete Note</p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+      Are you sure you want to delete the note?
+    </section>
+    <footer class="modal-card-foot is-justify-content-flex-end">
+      <div class="buttons">
+          <button class="button">Cancel</button>
+          <button class="button is-danger">Delete</button>
+      </div>
+    </footer>
+  </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
